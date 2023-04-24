777ab331abd07b4df7b7796288ef8ec78233122d
Robert Santos <robertdosantos09@gmail.com>
2023-04-24 12:51:46 -0300

    feat: Adicionando o redux-persist.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/index.css'
-import { store } from './redux/store'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { store, persistor } from "./redux/store"
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={ store }>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={ null } persistor={ persistor }>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 )
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import userSlice from "./user/slice";
 import emojisSlice from "./emoji/slice";
 
-export const store = configureStore({
-  reducer: {
-    user: userSlice,
-    emojis: emojisSlice
-  },
+const persistConfig = { key: 'root', storage }
+
+const rootReducers = combineReducers({
+  user: userSlice,
+  emojis: emojisSlice,
 });
+
+const reducers = persistReducer(persistConfig, rootReducers);
+
+export const store = configureStore({
+  reducer: reducers,
+});
+
+export const persistor = persistStore(store);
